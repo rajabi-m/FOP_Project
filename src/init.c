2358a45f8dc515b7a093959b11a7a0f996bfcafb
mahdi <fake@mail.huck>
2024-01-28 12:10:24 +0330

    changed how init works a little bit

--- src/init.c
+++ src/init.c
@@ -11,14 +11,14 @@ int GIT_Init(int argc, char **argv){
 
     // creating git dir
     mkdir(GIT_DIR_NAME, 0755);
-    createGitSubDir(LOCAL_CONFIG_DIR);
-    createGitSubDir(BRANCHES_DIR);
-    createGitSubDir(COMMITS_DIR);
-    createGitSubDir(OBJECTS_DIR);
+    mkdir(GIT_DIR_NAME "/" LOCAL_CONFIG_DIR, 0755);
+    mkdir(GIT_DIR_NAME "/" BRANCHES_DIR, 0755);
+    mkdir(GIT_DIR_NAME "/" COMMITS_DIR, 0755);
+    mkdir(GIT_DIR_NAME "/" OBJECTS_DIR, 0755);
 
     // creating git files
-    createGitSubFile(HEAD_FILE_NAME, MAIN_BRANCH);
-    createGitSubFile(STAGE_FILE_NAME, NULL);
+    mkfile(GIT_DIR_NAME "/" HEAD_FILE_NAME);
+    mkfile(GIT_DIR_NAME "/" STAGE_FILE_NAME);
 
 
     //
@@ -28,23 +28,23 @@ int GIT_Init(int argc, char **argv){
 }
 
 
-int createGitSubDir(const char *dir_name){
-    char *sub_dir = gigaStrcat(3, GIT_DIR_NAME,"/", dir_name);
-    int res = mkdir(sub_dir, 0755);
-    free(sub_dir);
-    return res;
-}
+// int createGitSubDir(const char *dir_name){
+//     char *sub_dir = gigaStrcat(3, GIT_DIR_NAME,"/", dir_name);
+//     int res = mkdir(sub_dir, 0755);
+//     free(sub_dir);
+//     return res;
+// }
 
-int createGitSubFile(const char *file_name, const char *default_value){
-    char *file_path = gigaStrcat(3, GIT_DIR_NAME, "/", file_name);
-    FILE *file = fopen(file_path, "w");
-    if (file == NULL){
-        return EXIT_FAILURE;
-    }
-    if (default_value){
-        fputs(default_value, file);
-    }
-    fclose(file);
-    free(file_path);
-    return EXIT_SUCCESS;
-}
+// int createGitSubFile(const char *file_name, const char *default_value){
+//     char *file_path = gigaStrcat(3, GIT_DIR_NAME, "/", file_name);
+//     FILE *file = fopen(file_path, "w");
+//     if (file == NULL){
+//         return EXIT_FAILURE;
+//     }
+//     if (default_value){
+//         fputs(default_value, file);
+//     }
+//     fclose(file);
+//     free(file_path);
+//     return EXIT_SUCCESS;
+// }
